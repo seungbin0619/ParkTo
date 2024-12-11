93e2d4c423657cac1e3a21db7f76ddafc4f9d836
Seungbin <seungbin0619@naver.com>
2024-12-11 16:05:23 +0900

    #4 feat: 카메라 설정 isometric으로 변경

--- Assets/Scripts/Game/PhysicsObject.cs
+++ Assets/Scripts/Game/PhysicsObject.cs
@@ -3,13 +3,13 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class PhysicsObject : MonoBehaviour
 {
-    public Rigidbody Rb { get; private set; }
+    public Rigidbody RB { get; private set; }
 
     [field: SerializeField]
     public Vector3 Size { get; private set; } = Vector3.one;
 
     void Awake() {
-        Rb = GetComponent<Rigidbody>();
+        RB = GetComponent<Rigidbody>();
     }
 
     protected virtual void OnCollisionEnter(Collision collision) {
